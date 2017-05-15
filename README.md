8446dba6068965bb4ecdcaf1fddf70b2e2c46578
Bogdan Rădulescu <bogdan@nimblex.net>
2017-05-15 01:04:00 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is just a sample one file PHP app that takes it's database connection parameters from environment variables.
+
+It's only purpose it's to demonstrate a Docker setup.
---
+++ web_data/index.php
@@ -0,0 +1,30 @@
+<?php
+
+$server = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$dbname = "testdatabase";
+
+// Create connection
+$conn = new mysqli($server, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+//echo "Connected successfully";
+
+$sql = "SELECT * FROM employees LIMIT 10";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Hire Date: " . $row["hire_date"]. " - Name: " . $row["first_name"]. " " . $row["last_name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+//phpinfo();
